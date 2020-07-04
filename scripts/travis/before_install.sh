f29b153abecbfd4eaecb8957df7bc88efabe979a
guozhibin <guozhibin2014@163.com>
2020-07-04 17:57:02 +0000

    update.

---
+++ scripts/travis/before_install.sh
@@ -0,0 +1,7 @@
+sudo apt-get update
+sudo apt-get install libgoogle-glog-dev
+
+# cmake
+# wget "https://github.com/Kitware/CMake/releases/download/v3.16.2/cmake-3.16.2-Linux-x86_64.tar.gz"
+# tar zxvf cmake-3.16.2-Linux-x86_64.tar.gz
+# export PATH=`pwd`/cmake-3.16.2-Linux-x86_64/bin:$PATH
