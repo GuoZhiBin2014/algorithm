c3930ea794d189a93f900e3266558e79dc0f8c39
guozhibin <guozhibin2014@163.com>
2020-07-04 18:29:39 +0000

    update.

--- scripts/travis/get_code_cov.sh
+++ scripts/travis/get_code_cov.sh
@@ -1,4 +1,8 @@
-for filename in `find . | egrep '\.cpp'`; 
-do 
-  gcov-5 -n -o . $filename > /dev/null; 
-done
+# Create lcov report
+# capture coverage info
+- lcov --directory . --capture --output-file coverage.info
+# filter out system and extra files.
+# To also not include test code in coverage add them with full path to the patterns: '*/tests/*'
+- lcov --remove coverage.info '/usr/*' "${HOME}"'/.cache/*' --output-file coverage.info
+# output coverage data for debugging (optional)
+- lcov --list coverage.info
