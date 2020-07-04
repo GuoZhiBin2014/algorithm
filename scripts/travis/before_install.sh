95bc966498673a177812463bad2b231e21fe38fb
guozhibin <guozhibin2014@163.com>
2020-07-04 19:02:41 +0000

    update.

--- scripts/travis/before_install.sh
+++ scripts/travis/before_install.sh
@@ -1,10 +1,11 @@
 sudo apt-get update
 sudo apt-get install libgoogle-glog-dev
+sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
+sudo apt-get update -qq
+sudo apt-get install -qq g++-5
+sudo update-alternatives --install /usr/bin/gcov gcov /usr/bin/gcov-5 90
 
 # cmake
 # wget "https://github.com/Kitware/CMake/releases/download/v3.16.2/cmake-3.16.2-Linux-x86_64.tar.gz"
 # tar zxvf cmake-3.16.2-Linux-x86_64.tar.gz
 # export PATH=`pwd`/cmake-3.16.2-Linux-x86_64/bin:$PATH
-
-# environment
-export CODECOV_TOKEN="9a834efa-6a22-4d47-807d-da28b6692e32"
--- scripts/travis/get_code_cov.sh
+++ scripts/travis/get_code_cov.sh
@@ -1,8 +1,5 @@
-# Create lcov report
-# capture coverage info
-lcov --directory . --capture --output-file coverage.info
-# filter out system and extra files.
-# To also not include test code in coverage add them with full path to the patterns: '*/tests/*'
-lcov --remove coverage.info '/usr/*' "${HOME}"'/.cache/*' --output-file coverage.info
-# output coverage data for debugging (optional)
-lcov --list coverage.info
+#!/bin/bash
+for filename in `find . | egrep '\.cpp'`; 
+do 
+  gcov-5 -n -o . $filename > /dev/null; 
+done
